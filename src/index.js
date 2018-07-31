be4d8a2731cbb3f9ec4bb72c84804c3e8a71f411
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2018-07-31 09:57:54 +0200

    Add a proper index for exporting the library

---
+++ src/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  EntityStatement: require('./EntityStatement'),
+  EntityStatementSigner: require('./EntityStatementSigner'),
+  JWKS: require('./JWKS'),
+  ResolvedMetadata: require('./ResolvedMetadata'),
+  SignedEntityStatement: require('./SignedEntityStatement'),
+  TrustChain: require('./TrustChain'),
+  ESFetcher: require('./ESFetcher')
+}
