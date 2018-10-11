6c3c4835a0c944751d6aac3c5d8418c553b8c484
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2018-10-11 11:20:39 +0200

    Update lookup script to use keys in jwks and add a new trust root for rolands test

--- lookup.js
+++ lookup.js
@@ -8,31 +8,60 @@ const
 
 let entityid = process.argv[3]
 let usage = process.argv[2]
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 const trustroot = [
-    {
-        "sub": "https://edugain.andreas.labs.uninett.no/openid",
-        "subTypes": ["openid_provider", "openid_client"],
-        "metadata": {
-          "openidClient": {
-            "special": true
-          }
-        },
-        "jwks": [
-          {
-            "kty": "RSA",
-            "use": "sig",
-            "alg": "RS256",
-            "n": "qnd5_krrHKzuJzb5_YEt4sP-YOGSbfVL_g06h1Q-q0nzTsO8MwtWVQx1nuR1cV-ruNwF2sFFGRNejVNKOxL8n5tGuYgJBRJBB5KcbnvRqSEMpObJxQzQuQrzxXFqMlmVRaaCINL5qFWTmdJz79cPleBBPr9DsD9O-nDSGV-R0LT3YWH0SrY5cEDVasUhWnFRY5eOTMRtxUB2m8FXBaZVAlIr5-Gy-SaTmybKQJ74iVpG16Hbw4t0tw14ReEO0aAsDq24cU7bHOueWnxZPfOltueZnIEKe3_eAmh-fieLvkkZSKqXRWKg_tZDbnjUqWH2lVvC2ReEOrns971V0Hjcbw",
-            "e": "AQAB",
-            "key_ops": [
-              "verify"
-            ],
-            "ext": true,
-            "kid": "edugain"
-          }
-        ]
+  {
+    "sub": "https://edugain.andreas.labs.uninett.no/openid",
+    "metadata": {
+      "openid_client": {
+        "special": true
+      }
+    },
+    "jwks": {
+      "keys": [
+        {
+          "kty": "RSA",
+          "use": "sig",
+          "alg": "RS256",
+          "n": "qnd5_krrHKzuJzb5_YEt4sP-YOGSbfVL_g06h1Q-q0nzTsO8MwtWVQx1nuR1cV-ruNwF2sFFGRNejVNKOxL8n5tGuYgJBRJBB5KcbnvRqSEMpObJxQzQuQrzxXFqMlmVRaaCINL5qFWTmdJz79cPleBBPr9DsD9O-nDSGV-R0LT3YWH0SrY5cEDVasUhWnFRY5eOTMRtxUB2m8FXBaZVAlIr5-Gy-SaTmybKQJ74iVpG16Hbw4t0tw14ReEO0aAsDq24cU7bHOueWnxZPfOltueZnIEKe3_eAmh-fieLvkkZSKqXRWKg_tZDbnjUqWH2lVvC2ReEOrns971V0Hjcbw",
+          "e": "AQAB",
+          "key_ops": [
+            "verify"
+          ],
+          "ext": true,
+          "kid": "edugain"
+        }
+      ]
     }
+  },
+  {
+    "sub": "https://agaton-sax.com:6000/alpha",
+    "metadata": {
+      "openid_client": {
+        "special": true
+      }
+    },
+    "jwks": {
+      "keys": [
+        {
+          "kty": "RSA",
+          "use": "sig",
+          "kid": "alpha1",
+          "e": "AQAB",
+          "n": "6B5BoSGfQ3dNvsNQ6U5Jph0wqszNNXAsEcMarjOUdSUWg7ayHJ4x0w62qZ4eGH1nFcj5OtlYL354fyfHOsAOUhiAHDrd9g6NZNLgA5QjykT1zwCHky8dXP6GfUrkrhVouIo5ynVWNqtxVMDsAtwYblqevKeNpVfBGNBV79FiIQPBboqOG8jo1cOthXNfmHsTdmKRETr385B1Hbz1jBpXA6MnZcCF3mwBueHJrSuRV4UFKf7MdRhcUYx9lDXGpe7lCijh1dfPd-Qn5sfTCljyKnad26MZJmYYLddIo1Fuo0r6cHjnuEMoQp7PdwCZNyOo84M2i4DFCOVEgjfEBRioMw"
+        },
+        {
+          "kty": "EC",
+          "use": "sig",
+          "kid": "alpha2",
+          "crv": "P-256",
+          "x": "NTKbxqcPqk30mkxwNqTN90DYkE4baXUoonbGionNHVo",
+          "y": "zlHh5u8D7E9Sfff586he6f8Ij_WTW29vKDZ3tH_hE7E"
+        }
+      ]
+    }
+  }
 ]
 
 if (!entityid || !usage) {
