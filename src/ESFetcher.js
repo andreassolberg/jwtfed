80f061bf8207b65ba162d891a05b7149a1a08906
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2018-10-11 11:26:11 +0200

    Use wellknown instead of webfinger, and use new authority hints structure

--- src/ESFetcher.js
+++ src/ESFetcher.js
@@ -14,30 +14,34 @@ class ESFetcher {
   fetchES(url) {
     return rp({url})
       .then((data) => {
-        return new SignedEntityStatement(data)
+        let eslist = JSON.parse(data)
+        return new SignedEntityStatement(eslist[0])
       })
   }
 
   fetch(sub, iss) {
     iss = iss || sub
-    let webfingerEndpoint = new URL('/.well-known/webfinger', iss)
-    let rel = 'http://oauth.net/specs/federation/1.0/entity'
-    console.log("Performing a WebFinger request to " + webfingerEndpoint + " with this queried resource " + sub)
+    let federationAPIendpoint = new URL('/.well-known/openid-federation', iss)
+    console.log("Performing a Federation API requwest request to " + federationAPIendpoint + " with this queried resource ")
+    console.log("Iss: " + iss)
+    console.log("Sub: " + sub)
     return rp({
-      url: webfingerEndpoint,
+      url: federationAPIendpoint,
       qs: {
-        rel: rel,
-        resource: sub
+        iss, sub
       }
     })
       .then((data) => {
-        console.log(" --- data ---")
-        console.log(data)
-        let res = JSON.parse(data)
-        if (res.links.length < 1) {
-          throw new Error("No links provided in LRD")
-        }
-        return this.fetchES(res.links[0].href)
+        // console.log(" --- data ---")
+        // console.log(data)
+        let eslist = JSON.parse(data)
+        return new SignedEntityStatement(eslist[0])
+
+        // let res = JSON.parse(data)
+        // if (res.links.length < 1) {
+        //   throw new Error("No links provided in LRD")
+        // }
+        // return this.fetchES(res.links[0].href)
       })
       .catch((err) => {
         console.error("Error fetching webfinger data form "+ webfingerEndpoint + " with this queried resource " + sub + "   error=" + err)
@@ -47,14 +51,15 @@ class ESFetcher {
 
   fetchChained(sub, iss) {
     let eslist = []
-    // console.log("Contacting issuer [" + iss + "] to requeset statements wrt [" + sub +"]" )
+    console.log("Contacting issuer [" + iss + "] to requeset statements wrt [" + sub + "]" )
     return this.fetch(sub, iss)
       .then((ses) => {
         eslist.push(ses)
-        console.log("----")
-        console.log(ses.decoded)
-        if (ses.decoded.payload.authority_hints && ses.decoded.payload.authority_hints.length) {
-          return Promise.all(ses.decoded.payload.authorityHints.map(hint => this.fetchChained(ses.decoded.payload.iss, hint)))
+        // console.log("----")
+        // console.log(ses.decoded)
+        let hints = ses.getHints()
+        if (hints.length > 0) {
+          return Promise.all(hints.map(hint => this.fetchChained(ses.decoded.payload.iss, hint)))
             .then((list) => {
               return eslist.concat([].concat.apply([], list))
             })
